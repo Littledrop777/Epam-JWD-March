663aed6b3d160987b6a4b341c2a607f3e4ba3bd8
Littledrop777 <Littledrop777@gmail.com>
2021-04-22 17:49:43 +0300

    refactoring

--- src/com/epam/jwd/service/Util.java
+++ src/com/epam/jwd/service/Util.java
@@ -3,28 +3,6 @@ package com.epam.jwd.service;
 import com.epam.jwd.model.Point;
 
 public class Util {
-    public static void sort(double[] array) {
-
-        double temp;
-        boolean check;
-        int count = array.length - 1;
-
-        do {
-            check = false;
-
-            for (int i = 0; i < count; i++) {
-                if (array[i] > array[i + 1]) {
-                    temp = array[i];
-                    array[i] = array[i + 1];
-                    array[i + 1] = temp;
-
-                    check = true;
-                }
-
-            }
-            count--;
-        } while (check);
-    }
 
     public static double calcLength(Point a, Point b) {
         double result;
